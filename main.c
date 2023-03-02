c0122d34f98a53845ba923186beb88a2645f12c8
Your Name <you@example.com>
2023-03-02 16:47:44 -0500

    fixed ugly string syntax

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/syscall.h>
 
 int main(int argc, char** argv) 
@@ -10,13 +11,24 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    // parse arguments
     for (int i=1; i < argc; i++) {
-        if (argv[i][0] != '"' && argv[i][0] != '\'') {
+        char is_int = 1;
+        int convi = atoi(argv[i]);
+        
+        // check if the entire string contains non-int chars
+        for (int j=0; argv[i][j] != '\x00'; j++) {
+            char chr = argv[i][j];
+            if (!(chr >= 48 && chr <= 57))
+            {
+                is_int = 0;
+                break;
+            }
+        }
+
+        // 0x0 is edge case
+        if (convi != 0 || is_int == 1) {
             argv[i] = atoi(argv[i]);
-        } else {
-            // cut off " and '
-            argv[i] = argv[i]+1;
-            argv[i][strlen(argv[i])-1] = '\x00';
         }
     }
 
@@ -30,7 +42,7 @@ int main(int argc, char** argv)
     }
 
     // miracously this does not raise any errors and syscalls gracefully accept invalid rest arguments
-    unsigned long result = syscall(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7]);
+    unsigned long result = syscall(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], argv[8]);
     printf("0x%lx\n", result);
     return 0;
 }
